07920afe2850db6977cdcc1248825745bbb4f6dc
Shawn <37581577+shupengcheng@users.noreply.github.com>
2024-08-08 17:22:38 +0800

    Add files via upload

---
+++ Source/UE427_MyVideoEncoder/ActorVideoEncoder.h
@@ -0,0 +1,56 @@
+// caidu 20220512
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "VideoEncoder.h"
+#include "ActorVideoEncoder.generated.h"
+
+
+
+UCLASS()
+class UE427_MYVIDEOENCODER_API AActorVideoEncoder : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AActorVideoEncoder();
+
+	// For Send Msg
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	class AActorWebSocketServer* WebSocketServer;
+
+	//场景捕获组件
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USceneCaptureComponent2D* SceneCaptureComponent2D;
+
+	//是否是全屏捕获
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bFullScreenCapture = false;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void BeginDestroy() override;
+
+private:
+	FTimespan StartTime = 0;
+	AVEncoder::FVideoEncoder::FLayerConfig VideoConfig;
+	TUniquePtr<AVEncoder::FVideoEncoder> VideoEncoder;
+	TSharedPtr<AVEncoder::FVideoEncoderInput> VideoEncoderInput;
+	TMap<AVEncoder::FVideoEncoderInputFrame*, FTexture2DRHIRef> InputFrameTextureMap;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable)
+	void StopWrite();
+
+private:
+	void CreateEncoder();
+	void OnEncodedVideoFrame(uint32 LayerIndex, const AVEncoder::FVideoEncoderInputFrame* Frame, const AVEncoder::FCodecPacket& Packet);
+	void OnFrameBufferReady(SWindow& SlateWindow, const FTexture2DRHIRef& FrameBuffer);
+	void ProcessVideoFrame(const FTexture2DRHIRef& FrameBuffer);
+	AVEncoder::FVideoEncoderInputFrame* ObtainInputFrame();
+	FTimespan GetMediaTimestamp() const;
+	void CopyTexture(const FTexture2DRHIRef& SourceTexture, FTexture2DRHIRef& DestinationTexture) const;
+};
